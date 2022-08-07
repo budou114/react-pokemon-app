547a656f239472524f75f86c44e3563f02ab43ba
budou114 <58349030+budou114@users.noreply.github.com>
2022-08-07 12:16:50 +0900

    簡単にナビゲーションバーを作成しよう

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import  Card  from './components/Card/Card';
+import Card from './components/Card/Card';
+import Navbar from './components/Navbar/Navbar';
 import { getAllPokemon, getPokemon } from './utils/pokemon';
 
 function App() {
@@ -36,7 +37,7 @@ function App() {
 
   return (
     <>
-
+      <Navbar />
       <div className="App">
         {loading ? (
           <h1>ロード中・・・</h1>
