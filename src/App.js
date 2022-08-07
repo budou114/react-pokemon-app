e6a626b21efacda606f0cf5a156800904a51a10e
budou114 <58349030+budou114@users.noreply.github.com>
2022-08-07 11:07:45 +0900

    ポケモンの詳細なデータを取得してみよう

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
 
   const [loading, setLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState([]);
 
   useEffect(() => {
     const fetchPokemonData = async () => {
@@ -19,15 +20,18 @@ function App() {
     fetchPokemonData();
   }, []);
 
-  const loadPokemon = (data) => {
-    let _pokemonData = Promise.all(
+  const loadPokemon = async (data) => {
+    let _pokemonData = await Promise.all(
       data.map((pokemon) => {
         // console.log(pokemon);
         let pokemonRecord = getPokemon(pokemon.url);
         return pokemonRecord;
       })
-    )
+    );
+    setPokemonData(_pokemonData);
   };
+
+  console.log(pokemonData);
 
   return (
     <div className="App">
